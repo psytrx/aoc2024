06c75481ebd13d005284ddffb3ad51397596293b
psytrx <fj.dumont@gmail.com>
2024-12-01 11:07:34 +0100

    prepare for day 2

---
+++ lib/day_02.ex
@@ -0,0 +1,13 @@
+defmodule Day02 do
+  def part_1(filename) do
+    :none
+  end
+
+  def part_2(filename) do
+    :none
+  end
+
+  def parse(filename) do
+    :none
+  end
+end
--- test/aoc2024_test.exs
+++ test/aoc2024_test.exs
@@ -1,9 +1,13 @@
 defmodule Aoc2024Test do
   use ExUnit.Case
-  doctest Day01
 
   test "day_01" do
     assert Day01.part_1("./input/01-test.txt") == 11
     assert Day01.part_2("./input/01-test.txt") == 31
+  end
+
+  test "day_02" do
+    assert Day02.part_1("./input/02-test.txt") == :unknown
+    assert Day02.part_2("./input/02-test.txt") == :unknown
   end
 end
