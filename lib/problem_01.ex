74946480bfd8efcfeca2f1e4e914d5a748b4ba2b
psytrx <fj.dumont@gmail.com>
2024-12-01 10:48:18 +0100

    solve day 1 part 2

--- lib/problem_01.ex
+++ lib/problem_01.ex
@@ -16,4 +16,25 @@ defmodule Problem01 do
     |> Enum.map(fn {a, b} -> abs(a - b) end)
     |> Enum.sum()
   end
+
+  def part_2(filename) do
+    {left_list, right_list} =
+      File.read!(filename)
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        line
+        |> String.split()
+        |> Enum.map(&String.to_integer/1)
+        |> List.to_tuple()
+      end)
+      |> Enum.unzip()
+
+    # Create frequency map of right list
+    right_frequencies = Enum.frequencies(right_list)
+
+    # For each number in left list, multiply by its frequency in right list
+    left_list
+    |> Enum.map(fn num -> num * Map.get(right_frequencies, num, 0) end)
+    |> Enum.sum()
+  end
 end
